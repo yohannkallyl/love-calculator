034b7b278812d7e208ac7d22c66b8d75fc4646a5
Yohann Kallyl <kallylyohann@gmail.com>
2024-04-16 21:37:29 -0300

    daily update

--- script/script.js
+++ script/script.js
@@ -1,7 +1,10 @@
+
+
 //parte da animação .card
 
 document.addEventListener("DOMContentLoaded", function(){
     document.querySelector(".card").classList.add("show");
+    
 
 });
 
@@ -13,12 +16,46 @@ window.onload = function(){
 function calculoAmor(){
     let seuNome = document.getElementById("seu-nome").value
     let nomeCrush = document.getElementById("nome-crush").value
+    let resultadoMensagem = document.getElementById("resultado-mensagem")
+    let resultadoPorcentagemElement = document.getElementById("resultado-porcentagem")
+    let resultadoPorcentagem = resultadoPorcentagemElement.value
+    
 
+    resultadoMensagem.innerHTML = "" //resetar
+    
+    
     if(seuNome.length === 0 || nomeCrush.length === 0){
-        alert("[ERRO] Faltam informações!")
+        resultadoMensagem.innerHTML = "Verifique os campos e tente novamente!"
+    } else if(seuNome === nomeCrush){
+        resultadoMensagem.innerHTML = "Epa, amigão. Quer namorar contigo mesmo? KKKKKK"
     } else{
-        alert("Tudo certo")
+        let randomNumber = Math.random()
+        resultadoPorcentagem = Math.floor(randomNumber * 100) + 1
+        resultadoPorcentagemElement.innerHTML = `${resultadoPorcentagem}% de chance de dar certo`
+
+        if(resultadoPorcentagem <= 49){
+            resultadoMensagem.innerHTML = 'Tá bem díficil. "Sonhar, nunca desistir 🎹"'
+        } else if(resultadoPorcentagem >= 50 && resultadoPorcentagem <= 69){
+            resultadoMensagem.innerHTML = "Será que dá? Tem que tentar 🤔"
+        } else if(resultadoPorcentagem >= 70 && resultadoPorcentagem <= 99){
+            resultadoMensagem.innerHTML = "Chance boa demais, hein. Continua assim e vai administrando 👀"
+        } else if(resultadoPorcentagem === 100){
+            document.getElementById("seu-nome").style.borderColor = "#d000ff"
+            document.getElementById("nome-crush").style.borderColor = "#d000ff"
+            resultadoMensagem.innerHTML = "Cara, vocês sinceramente nasceram um para o outro ❤️‍🔥"
+
+            
+            
+        } else{
+            return false
+            
+        }
+    }
+    
+    
+    
+
     }
 
-}
+
 
